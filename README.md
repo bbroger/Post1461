c9ee614fdb551745d86625f303c6a0b7ba5d888f
Rafael Couto <faelcalves@gmail.com>
2017-03-19 18:43:55 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+Uma breve introdução ao Composer
+
+Mais informações: http://rafaelcouto.com.br/uma-breve-introducao-ao-composer/
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Incluindo arquivo de autoload gerado pelo Composer
+require 'vendor/autoload.php';
+
+// Importando biblioteca para o arquivo
+use Stringy\Stringy;
+
+// Utilizando a uma função da biblioteca para criar um slug a partir de uma string
+// Retorno: uma-breve-introducao-ao-composer
+echo Stringy::create('Uma breve introdução ao Composer')->slugify();
