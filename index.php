70d76d8e57dc9631fb5faf25ba3e05fd22633f61
Rafael Couto <faelcalves@gmail.com>
2017-03-19 18:46:28 -0300

    Ajuste comentário

--- index.php
+++ index.php
@@ -6,6 +6,6 @@ require 'vendor/autoload.php';
 // Importando biblioteca para o arquivo
 use Stringy\Stringy;
 
-// Utilizando a uma função da biblioteca para criar um slug a partir de uma string
+// Utilizando uma função da biblioteca para criar um slug a partir de uma string
 // Retorno: uma-breve-introducao-ao-composer
 echo Stringy::create('Uma breve introdução ao Composer')->slugify();
